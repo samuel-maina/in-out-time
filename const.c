6ca277a5de61cdbc45b5bb8bb142d8f42da0ef94
samuel-maina <40397814+samuel-maina@users.noreply.github.com>
2019-01-26 00:17:56 +0300

    Add files via upload

---
+++ const.c
@@ -0,0 +1,13 @@
+//const keyword
+//const type variable
+#include<stdio.h>
+int main()
+{
+    const int LENGTH = 10;
+    const int WIDTH=5;
+    const int NEWLINE = '\n';
+    int area;
+    printf("value of the are %d",area);
+    printf("%c",NEWLINE);
+    return 0;
+}
